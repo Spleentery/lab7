69987e058cb999d805ece5f779e24f067b3e40ef
david <david78787878787@gmail.com>
2021-12-09 18:17:01 +0100

    An abstract class is a class that is designed to be specifically used as a base class. Polymorphism

--- lab7/cmake-build-debug/CMakeFiles/lab7.dir/DependInfo.cmake
+++ lab7/cmake-build-debug/CMakeFiles/lab7.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/David/Desktop/Programming stuff/cLion/lab7/main.cpp" "C:/Users/David/Desktop/Programming stuff/cLion/lab7/cmake-build-debug/CMakeFiles/lab7.dir/main.cpp.obj"
+  "C:/Users/David/Desktop/Programming stuff/GitHub/lab7/lab7/main.cpp" "C:/Users/David/Desktop/Programming stuff/GitHub/lab7/lab7/cmake-build-debug/CMakeFiles/lab7.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
-
+const double pi = 3.14159265359;
 class figura{
     string m_nazwa;
     string m_color;
 public:
-    figura(string nazwa,string color):m_nazwa(nazwa),m_color(color){
-
+    figura(string nazwa = "default",string color = "default"):m_nazwa(nazwa),m_color(color){
     }
     string getnazwa(){
         return m_nazwa;
@@ -17,16 +16,19 @@ public:
     void printFigura(){
         cout << getnazwa() << "\t" << getcolor() << endl;
     }
+    virtual double area() = 0;
+
+
 };
 
 class square : public figura{
-protected:
+public:
     double m_a;
     double m_b;
-public:
-    square(double a, double b, string name = "square"): m_a(a),m_b(b), figura(name,"red"){
+
+    square(double a = 2, double b = 3, string name = "square"): m_a(a),m_b(b), figura(name,"red"){
     }
-    double areaSquare(){
+    virtual double area() {
         return this->m_b* this->m_a;
     }
     void printSquare(){
@@ -34,29 +36,52 @@ public:
         cout << m_a << ", " << m_b << endl;
     }
 };
-class triangle : protected square{
+class triangle : public square{
 private:
     double m_h;
 public:
-    triangle(double h, double a,double b, string name = "Triangle"):m_h(h), square(a,b,name){
+    triangle(double h = 2, double a = 2,double b = 3, string name = "Triangle"):m_h(h), square(a,b,name){
     }
-    double areaTriangle(){
-        return areaSquare()*0.5;
+    virtual double area() {
+        return square::area()*0.5;
     }
     void printTriangle(){
         printSquare();
         cout << "and height: "<< m_h << endl;
     }
 };
+class Circle : public figura{
+private:
+    double m_radius;
+public:
+    Circle(double radius):m_radius(radius), figura("circle","no color, wtf"){
+
+    }
+    virtual double area(){
+        printFigura();
+        return (m_radius*m_radius*pi);
+    }
+};
+class Rectangle :  public figura{
+private:
+    double m_a;
+    double m_b;
+public:
+    Rectangle(double a,double b):m_a(a),m_b(b), figura("rectangle","color? what"){
+
+    }
+    virtual double area(){
+        return m_a*m_b;
+    }
+
+};
 int main( int argc, char *argv[] ) {
-   figura a("figura1", "purple");
-   a.printFigura();
-   square b(5,3);
-   b.printSquare();
-   cout << b.areaSquare() << endl;
-   triangle c(2,5,3);
-   c.printTriangle();
-   cout << "area of Triangle: " << c.areaTriangle() << endl;
+
+     figura *circle = new Circle(5);
+    figura *rectangle = new Rectangle(5, 18);
+     cout << "Circle area: " << circle->area() << endl;
+    cout << "Rectangle area: " << rectangle->area() << endl;
+
 
 	return 0;
 }
